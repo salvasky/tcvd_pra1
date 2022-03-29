084dc974dcc4809d5faab35365d7371b86faf0cf
salvasky <ssanchisb@uoc>
2022-03-29 16:27:47 +0200

    updated comments

--- mdf.py
+++ mdf.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
 import time
 import os.path
 
@@ -15,19 +13,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 page_2 = requests.get(url_2)
 soup_2 = BeautifulSoup(page_2.content, 'html.parser')
 
-# print(soup.prettify())
 with open('test_mdf.html', 'w') as f:
     f.write(soup.prettify())
 
 with open('test_mdf_confi.html', 'w') as f:
     f.write(soup_2.prettify())
 
-# La inspecció dels HTML mostra que hi ha scroll infinit a les dues pàgines.
-# Informació sobre scraping amb infinit scrolling with selenium:
-# https://medium.com/analytics-vidhya/using-python-and-selenium-to-scrape-infinite-scroll-web-pages-825d12c24ec7
-# https://github.com/LimonSafayet/Web-Scraping-in-Python
-
-# Proves de navegació a una pàgina amb scroll infinit
+# Navegació a una pàgina amb scroll infinit
 profile = webdriver.FirefoxProfile()
 profile.set_preference("general.useragent.override",
                        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0")
@@ -45,11 +37,6 @@ while True:
     last_height = new_height
 
 soup_inf = BeautifulSoup(driver.page_source, 'html.parser')
-# soup = BeautifulSoup(driver.page_source, 'lxml')
-# html parser ha funcionat
-
-# with open("test_inf.html", 'w') as f:
-#     f.write(soup_inf.prettify())
 
 # Cada entrada al blog va delimitada pels tags <article> i disposa de la seva url
 # Recollir totes les URLs, accedir-hi una a una i extreure la informació d'interès
@@ -71,6 +58,7 @@ for article in soup_body.find_all('article'):
 # Prova d'impressió de les dates dels posts
 print(titols)
 
+# Creació d'un fitxer que emmagatzema les fotos de les artistes en format jpg
 noms = titols
 
 os.mkdir('fotos')
@@ -83,5 +71,3 @@ for i in image:
     file.write(response.content)
     file.close()
     noms.pop(0)
-
-
